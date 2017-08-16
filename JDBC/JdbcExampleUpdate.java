879a41bb99dd317c11e6e3d55a5075f69c557b4b
VeryFatBoy <VeryFatBoy@users.noreply.github.com>
2017-08-16 15:06:05 +0100

    Create JdbcExampleUpdate.java

---
+++ JDBC/JdbcExampleUpdate.java
@@ -0,0 +1,28 @@
+package org.apache.ignite.examples.datagrid;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JdbcExampleUpdate {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("JDBC example started.");
+
+        // Register JDBC driver
+        Class.forName("org.apache.ignite.IgniteJdbcDriver");
+
+        // Open JDBC connection
+        Connection conn = DriverManager.getConnection(
+                "jdbc:ignite:thin://127.0.0.1/");
+
+        // Update
+        try (Statement stmt = conn.createStatement()) {
+
+            // Update city
+            stmt.executeUpdate("UPDATE city SET name = 'Foster City' WHERE id = 2");
+        }
+
+        System.out.println("Updated data.");
+    }
+}
